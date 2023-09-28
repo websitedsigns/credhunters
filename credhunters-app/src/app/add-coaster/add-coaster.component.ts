0509a0d27b939a78aaa9cac8890a62e715866b6d
websitedsigns <websitedsigns@gmail.com>
2023-09-28 18:21:57 +0100

    nav bar running through app

--- credhunters-app/src/app/add-coaster/add-coaster.component.ts
+++ credhunters-app/src/app/add-coaster/add-coaster.component.ts
@@ -1,60 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 import { CoasterDataService } from '../coaster-data.service';
 
 @Component({
   selector: 'app-add-coaster',
   templateUrl: './add-coaster.component.html',
-  animations: [
-    trigger('buttonAnimation', [
-      state('normal', style({
-        transform: 'scale(1)',
-      })),
-      state('clicked', style({
-        transform: 'scale(1.1)',
-      })),
-      transition('normal => clicked', [
-        animate('200ms ease-out')
-      ]),
-      transition('clicked => normal', [
-        animate('200ms ease-in')
-      ]),
-    ]),
-  ],
 })
 export class AddCoasterComponent implements OnInit {
   coasterName: string = '';
   park: string = '';
   manufacturer: string = '';
   buttonState: string = 'normal';
-  coasterCount: number = 0; // Initialize the coaster count
+  coasterCount: number = 0;
 
   constructor(private coasterDataService: CoasterDataService) {}
 
   ngOnInit() {
-    // Get the initial coaster count from the data service
+    // Initialize the coaster count when the component is loaded
     this.coasterCount = this.coasterDataService.getCoasterCount();
   }
 
   addCoaster() {
-    const newCoaster = {
-      name: this.coasterName,
-      park: this.park,
-      manufacturer: this.manufacturer,
-    };
+    // Check if the form fields are filled and not duplicate
+    if (this.coasterName && this.park && this.manufacturer) {
+      const newCoaster = {
+        name: this.coasterName,
+        park: this.park,
+        manufacturer: this.manufacturer,
+      };
 
-    // Add the coaster to the data service
-    this.coasterDataService.addCoaster(newCoaster);
+      // Add the coaster to the data service
+      this.coasterDataService.addCoaster(newCoaster);
 
-    // Update the coaster count
-    this.coasterCount++;
+      // Reset form fields
+      this.coasterName = '';
+      this.park = '';
+      this.manufacturer = '';
 
-    // Set button animation state
-    this.buttonState = 'clicked';
-
-    // Reset form fields
-    this.coasterName = '';
-    this.park = '';
-    this.manufacturer = '';
+      // Update the coaster count
+      this.coasterCount++;
+    }
   }
 }
